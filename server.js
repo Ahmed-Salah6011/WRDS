ecd691f87a94c19abe341c98066f220a096bb2cb
Mark_Sameh <aukshmark@gmail.com>
2021-06-16 23:05:37 +0200

    may be the last

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const multer = require("multer");
 const FileReader = require("filereader");
 const { spawn } = require("child_process");
 const path = require("path");
+const bodyparser = require("body-parser");
 const socketio = require("socket.io");
-const { parse } = require("path");
 const server = http.createServer(app);
 const io = socketio(server);
 ///////////////////////////////////////
 app.use(express.static("uploads"));
 app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 const port = 4000;
 let baseDir = path.join(__dirname, "videos", "test.MKV");
@@ -25,6 +26,22 @@ var storage = multer.diskStorage({
     cb(null, file.fieldname + ".jpg");
   },
 });
+
+var check_for_id = (id) => {
+  let raw_data;
+  try {
+    raw_data = fs.readFileSync("db.json", (encoding = "utf-8"));
+  } catch {
+    return "not found";
+  }
+  console.log(raw_data);
+  let db_data = JSON.parse(raw_data);
+  for (let index = 0; index < db_data.table.length; index++) {
+    if (db_data.table[index].id == id) return db_data.table[index];
+  }
+  return "not found";
+};
+
 const upload = multer({
   dest: path.join(__dirname, "uploads"),
   storage: storage,
@@ -57,26 +74,61 @@ app.post("/uploadfile", uploadMultiple, function (req, res, next) {
 });
 
 app.get("/ocr", (req, res) => {
-  let raw_id = fs.readFileSync("ID.json",encoding="utf-8");
-  let raw_cert = fs.readFileSync("CERT.json",encoding="utf-8");
-  let id_data = parse(raw_id);
-  let cert_data = parse(raw_cert);
-   console.log(id_data)
-   console.log(cert_data)
-  res.render("ocr", { 'cert': cert_data,'id':id_data });
-   
+  let raw_id = fs.readFileSync("ID.json", (encoding = "utf-8"));
+  let raw_cert = fs.readFileSync("CERT.json", (encoding = "utf-8"));
+  let id_data = JSON.parse(raw_id);
+  let cert_data = JSON.parse(raw_cert);
+  res.render("ocr", { cert: cert_data, id: id_data });
 });
 
 app.get("/video", (req, res) => {
   res.render("video_record");
 });
 ////////////////////////////////////
-app.post("/upload", upload.single("img"), (req, res) => {
-  const tempPath = req.file.path;
-  const targetPath = path.join(__dirname, "./uploads/image.jpg");
-  fs.rename(tempPath, targetPath, (err) => {
-    if (err) console.log(err);
-    res.render("video_record");
+// app.post("/upload", upload.single("img"), (req, res) => {
+//   const tempPath = req.file.path;
+//   const targetPath = path.join(__dirname, "./uploads/image.jpg");
+//   fs.rename(tempPath, targetPath, (err) => {
+//     if (err) console.log(err);
+//     res.render("video_record");
+//   });
+// });
+
+app.post("/show_data", (req, res) => {
+  let test = check_for_id(req.body.id);
+  if (test == "not found") {
+    res.send("id not found");
+  } else {
+    res.render("show_data", { data: test });
+  }
+});
+
+app.post("/ocr", (req, res) => {
+  fs.readFile("db.json", "utf8", function readFileCallback(err, data) {
+    var obj;
+    let test = check_for_id(req.body.id);
+    if (test == "not found") {
+      if (err) {
+        obj = {
+          table: [],
+        };
+        console.log(req.body);
+        obj.table.push(req.body);
+        let json = JSON.stringify(obj); //convert it to json
+        fs.writeFile("db.json", json, "utf8", () => {
+          res.send("done");
+        }); // write it
+      } else {
+        obj = JSON.parse(data); //now it an object
+        obj.table.push(req.body); //add some data
+        let json = JSON.stringify(obj); //convert it back to json
+        fs.writeFile("db.json", json, "utf8", () => {
+          res.send("done");
+        }); // write it back
+      }
+    } else {
+      res.render("show_data", { data: test });
+    }
   });
 });
 //////////////////////////////////
